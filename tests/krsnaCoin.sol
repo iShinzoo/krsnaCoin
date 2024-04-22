7e5770b1a4e21fb571c0a143d177fab0cce08c5e
iShinzoo <krishe7t8rr@gmail.com>
2024-04-22 18:56:41 +0530

    initial

---
+++ tests/krsnaCoin.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20{
+    
+    constructor () ERC20 ("KRSNA", "TOKEN"){
+        _mint(msg.sender, 10000 * ( 10 ** uint256(decimals())));
+    }
+
+}
